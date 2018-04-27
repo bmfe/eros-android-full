03c23ef6b08f431abd09d3baa833990516db762d
limingyi <11111111>
2018-04-27 11:17:40 +0800

    mod:first commit

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+echo "开始更新依赖库"
+rm -r wxframework/
+rm -r sdk/
+rm -r nexus/
+rm -r ErosPluginAmap/
+rm -r erosplugingt/
+rm -r erospluginwxpay/
+rm -r erospluginumeng/
+
+git clone https://github.com/bmfe/WeexErosFramework.git "wxframework" 
+
+git clone https://github.com/bmfe/WeexSDK.git "sdk" 
+
+git clone https://github.com/bmfe/eros-nexus.git "nexus" 
+
+git clone https://github.com/bmfe/eros-plugin-android-amap.git "ErosPluginAmap" 
+
+git clone git@github.com:bmfe/eros-plugin-android-getui.git "erosplugingt" 
+
+git clone git@github.com:bmfe/eros-plugin-android-wxpay.git "erospluginwxpay" 
+
+git clone git@github.com:bmfe/eros-plugin-android-wxshare.git "erospluginumeng" 
+cd ../
+
+echo "依赖库更新完成"
